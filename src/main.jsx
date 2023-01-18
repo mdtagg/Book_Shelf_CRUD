994789909a15cc29b58f891c16c02bef6d42d46f
Mike Tagg <mdtagg91@gmail.com>
2023-01-17 16:58:18 -0800

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppRouter from './AppRouter'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AppRouter />
+)
+
+
